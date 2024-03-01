3cf02430e186ecd4de2467fb124e80ad0bfb78ba
Celyn Walters <3299161+celynw@users.noreply.github.com>
2024-03-01 12:21:33 +0000

    Remove (hopefully) redundant lines

--- navidrome/rootfs/etc/cont-init.d/90-data_location.sh
+++ navidrome/rootfs/etc/cont-init.d/90-data_location.sh
@@ -4,17 +4,8 @@
 set -e
 
 # Configure app
-
-bashio::config.has_value 'ND_ENABLESHARING' && export ND_ENABLESHARING=$(bashio::config 'ND_ENABLESHARING') && bashio::log.info "ND_ENABLESHARING set to $ND_ENABLESHARING"
-bashio::config.has_value 'ND_MUSICFOLDER' && export ND_MUSICFOLDER=$(bashio::config 'ND_MUSICFOLDER') && bashio::log.info "ND_MUSICFOLDER set to $ND_MUSICFOLDER"
-bashio::config.has_value 'ND_DATAFOLDER' && export ND_DATAFOLDER=$(bashio::config 'ND_DATAFOLDER') && bashio::log.info "ND_DATAFOLDER set to $ND_DATAFOLDER"
-
-# Check data location
-LOCATION=$(bashio::config 'ND_DATAFOLDER')
-if !(bashio::config.has_value 'ND_DATAFOLDER'); then
-    bashio::log.warning "No ND_DATAFOLDER location was provided."
-    exit 1
-fi
+bashio::config.has_value "ND_MUSICFOLDER" && export ND_MUSICFOLDER=$(bashio::config "ND_MUSICFOLDER") && bashio::log.info "ND_MUSICFOLDER set to $ND_MUSICFOLDER"
+bashio::config.has_value "ND_DATAFOLDER" && export ND_DATAFOLDER=$(bashio::config "ND_DATAFOLDER") && bashio::log.info "ND_DATAFOLDER set to $ND_DATAFOLDER"
 
 # Create folder
 if [ ! -d "$ND_DATAFOLDER" ]; then
@@ -22,24 +13,11 @@ if [ ! -d "$ND_DATAFOLDER" ]; then
     mkdir -p "$ND_DATAFOLDER"
 fi
 
-# export ND_BASEURL=/api/hassio_ingress/nzZbBSf89wBkxXceoWvFTtvazRHU2wLp0I7Oaf1RAyY
-
-##############
-# LAUNCH APP #
-##############
-
-## We are in the container's FS here
-bashio::log.info "Looking inside /data"
-ls -la /data
-
-bashio::log.info "Looking inside /music"
-ls -la /music
-
+# We are in the container's FS here
 bashio::log.info "Looking inside ND_DATAFOLDER $ND_DATAFOLDER"
 ls -la $ND_DATAFOLDER
-
 bashio::log.info "Looking inside ND_MUSICFOLDER $ND_MUSICFOLDER"
 ls -la $ND_MUSICFOLDER
 
-bashio::log.info 'Starting Navidrome...'
+bashio::log.info "Starting Navidrome..."
 /app/navidrome
