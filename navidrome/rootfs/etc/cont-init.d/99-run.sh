1f23aa3b808598cfb106c453c6e411d970456aa1
Celyn Walters <3299161+celynw@users.noreply.github.com>
2024-03-02 16:21:58 +0000

    Fix bashrc

--- navidrome/rootfs/etc/cont-init.d/99-run.sh
+++ navidrome/rootfs/etc/cont-init.d/99-run.sh
@@ -49,46 +49,24 @@ export ND_UILOGINBACKGROUNDURL=$(bashio::config "UILoginBackgroundUrl") && bashi
 export ND_UIWELCOMEMESSAGE=$(bashio::config "UIWelcomeMessage") && bashio::log.info "ND_UIWELCOMEMESSAGE set to $ND_UIWELCOMEMESSAGE"
 
 # Export variables
-if [ -d /var/run/s6/container_environment ]; then
-	printf "%s" "$ND_LOGLEVEL" > /var/run/s6/container_environment/ND_LOGLEVEL
-	printf "%s" "$ND_ENABLEFAVOURITES" > /var/run/s6/container_environment/ND_ENABLEFAVOURITES
-	printf "%s" "$ND_ENABLEGRAVATAR" > /var/run/s6/container_environment/ND_ENABLEGRAVATAR
-	printf "%s" "$ND_ENABLESHARING" > /var/run/s6/container_environment/ND_ENABLESHARING
-	printf "%s" "$ND_ENABLESTARRATING" > /var/run/s6/container_environment/ND_ENABLESTARRATING
-	printf "%s" "$ND_JUKEBOX_DEFAULT" > /var/run/s6/container_environment/ND_JUKEBOX_DEFAULT
-	printf "%s" "$ND_JUKEBOX_DEVICES" > /var/run/s6/container_environment/ND_JUKEBOX_DEVICES
-	printf "%s" "$ND_JUKEBOX_ENABLED" > /var/run/s6/container_environment/ND_JUKEBOX_ENABLED
-	printf "%s" "$ND_LASTFM_APIKEY" > /var/run/s6/container_environment/ND_LASTFM_APIKEY
-	printf "%s" "$ND_LASTFM_ENABLED" > /var/run/s6/container_environment/ND_LASTFM_ENABLED
-	printf "%s" "$ND_LASTFM_SECRET" > /var/run/s6/container_environment/ND_LASTFM_SECRET
-	printf "%s" "$ND_LISTENBRAINZ_ENABLED" > /var/run/s6/container_environment/ND_LISTENBRAINZ_ENABLED
-	printf "%s" "$ND_SCANSCHEDULE" > /var/run/s6/container_environment/ND_SCANSCHEDULE
-	printf "%s" "$ND_SESSIONTIMEOUT" > /var/run/s6/container_environment/ND_SESSIONTIMEOUT
-	printf "%s" "$ND_SPOTIFY_ID" > /var/run/s6/container_environment/ND_SPOTIFY_ID
-	printf "%s" "$ND_SPOTIFY_SECRET" > /var/run/s6/container_environment/ND_SPOTIFY_SECRET
-	printf "%s" "$ND_TRANSCODINGCACHESIZE" > /var/run/s6/container_environment/ND_TRANSCODINGCACHESIZE
-	printf "%s" "$ND_UILOGINBACKGROUNDURL" > /var/run/s6/container_environment/ND_UILOGINBACKGROUNDURL
-	printf "%s" "$ND_UIWELCOMEMESSAGE" > /var/run/s6/container_environment/ND_UIWELCOMEMESSAGE
-fi
-
 {
-	printf "%s" "ND_LOGLEVEL=\"$ND_LOGLEVEL\""
-	printf "%s" "ND_ENABLEFAVOURITES=\"$ND_ENABLEFAVOURITES\""
-	printf "%s" "ND_ENABLEGRAVATAR=\"$ND_ENABLEGRAVATAR\""
-	printf "%s" "ND_ENABLESHARING=\"$ND_ENABLESHARING\""
-	printf "%s" "ND_ENABLESTARRATING=\"$ND_ENABLESTARRATING\""
-	printf "%s" "ND_JUKEBOX_DEFAULT=\"$ND_JUKEBOX_DEFAULT\""
-	printf "%s" "ND_JUKEBOX_DEVICES=\"$ND_JUKEBOX_DEVICES\""
-	printf "%s" "ND_JUKEBOX_ENABLED=\"$ND_JUKEBOX_ENABLED\""
-	printf "%s" "ND_LASTFM_APIKEY=\"$ND_LASTFM_APIKEY\""
-	printf "%s" "ND_LASTFM_ENABLED=\"$ND_LASTFM_ENABLED\""
-	printf "%s" "ND_LASTFM_SECRET=\"$ND_LASTFM_SECRET\""
-	printf "%s" "ND_LISTENBRAINZ_ENABLED=\"$ND_LISTENBRAINZ_ENABLED\""
-	printf "%s" "ND_SCANSCHEDULE=\"$ND_SCANSCHEDULE\""
-	printf "%s" "ND_SESSIONTIMEOUT=\"$ND_SESSIONTIMEOUT\""
-	printf "%s" "ND_SPOTIFY_ID=\"$ND_SPOTIFY_ID\""
-	printf "%s" "ND_SPOTIFY_SECRET=\"$ND_SPOTIFY_SECRET\""
-	printf "%s" "ND_TRANSCODINGCACHESIZE=\"$ND_TRANSCODINGCACHESIZE\""
-	printf "%s" "ND_UILOGINBACKGROUNDURL=\"$ND_UILOGINBACKGROUNDURL\""
-	printf "%s" "ND_UIWELCOMEMESSAGE=\"$ND_UIWELCOMEMESSAGE\""
+	printf "%s\n" "export ND_LOGLEVEL=\"$ND_LOGLEVEL\""
+	printf "%s\n" "export ND_ENABLEFAVOURITES=\"$ND_ENABLEFAVOURITES\""
+	printf "%s\n" "export ND_ENABLEGRAVATAR=\"$ND_ENABLEGRAVATAR\""
+	printf "%s\n" "export ND_ENABLESHARING=\"$ND_ENABLESHARING\""
+	printf "%s\n" "export ND_ENABLESTARRATING=\"$ND_ENABLESTARRATING\""
+	printf "%s\n" "export ND_JUKEBOX_DEFAULT=\"$ND_JUKEBOX_DEFAULT\""
+	printf "%s\n" "export ND_JUKEBOX_DEVICES=\"$ND_JUKEBOX_DEVICES\""
+	printf "%s\n" "export ND_JUKEBOX_ENABLED=\"$ND_JUKEBOX_ENABLED\""
+	printf "%s\n" "export ND_LASTFM_APIKEY=\"$ND_LASTFM_APIKEY\""
+	printf "%s\n" "export ND_LASTFM_ENABLED=\"$ND_LASTFM_ENABLED\""
+	printf "%s\n" "export ND_LASTFM_SECRET=\"$ND_LASTFM_SECRET\""
+	printf "%s\n" "export ND_LISTENBRAINZ_ENABLED=\"$ND_LISTENBRAINZ_ENABLED\""
+	printf "%s\n" "export ND_SCANSCHEDULE=\"$ND_SCANSCHEDULE\""
+	printf "%s\n" "export ND_SESSIONTIMEOUT=\"$ND_SESSIONTIMEOUT\""
+	printf "%s\n" "export ND_SPOTIFY_ID=\"$ND_SPOTIFY_ID\""
+	printf "%s\n" "export ND_SPOTIFY_SECRET=\"$ND_SPOTIFY_SECRET\""
+	printf "%s\n" "export ND_TRANSCODINGCACHESIZE=\"$ND_TRANSCODINGCACHESIZE\""
+	printf "%s\n" "export ND_UILOGINBACKGROUNDURL=\"$ND_UILOGINBACKGROUNDURL\""
+	printf "%s\n" "export ND_UIWELCOMEMESSAGE=\"$ND_UIWELCOMEMESSAGE\""
 } >> ~/.bashrc
